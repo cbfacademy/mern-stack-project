4b0604bb7b279c604a41a212b2a0ca1e5138fbf6
jbravoh <68926455+jbravoh@users.noreply.github.com>
2021-01-22 22:56:57 +0000

    Added button to search element

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "../stylesheets/About.css";
+import { Link } from "react-router-dom";
 
 function About() {
   return (
     <React.Fragment>
       <div className="about-container">
         <h1 className="about-header">About Us</h1>
-        <p className="about-paragraph">Welcome to Experience. This website is... </p>
+        <p className="about-paragraph">
+          Welcome to Experience. This website is...{" "}
+        </p>
+        <Link to="/">
+          <button className="about-button">Search Opportunities</button>
+        </Link>
       </div>
     </React.Fragment>
   );
