8ae73afb4eee587ea1f42996ba0a24eff2326d03
jbravoh <68926455+jbravoh@users.noreply.github.com>
2021-01-27 18:00:57 +0000

    cleaned up code

--- server/routes/clientRoutes.js
+++ server/routes/clientRoutes.js
@@ -1,21 +1,37 @@
 const mongoose = require("mongoose");
 const Client = mongoose.model("clients");
 
+
 module.exports = (app) => {
-  // Get's all clients/users
+  // GET - get clients/users
   app.get("/api/client", async (req, res) => {
-    const clients = await Client.find(); // calling function to get the data
+    const clients = await Client.find(); 
     return res.status(200).send(clients);
   });
 
   //POST - add a new clients/users
   app.post("/api/client", async (req, res) => {
-    const newClient = await Client.create(req.body); // contain anything on the form
+    const newClient = await Client.create(req.body); 
     return res.status(201).send({
       error: false,
       newClient,
     });
-  });
+});
+
+  // //POST route to register a user
+  // app.post('/api/client', function(req, res) {
+  //   const { email, username, password } = req.body;
+  //   const user = new user({ email, username, password });
+  //   user.save(function(err) {
+  //     if (err) {
+  //       res.status(500)
+  //         .send("Error registering new user please try again.");
+  //     } else {
+  //       res.status(200).send("Welcome to Experience!")
+  //     }
+  //   })
+  // });
+ 
 
   //LOGIN
 
